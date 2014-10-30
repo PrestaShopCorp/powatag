c9eb4fa17c3bf2ec082d0d2a9ef15bdaa0450a66
Thoma Biguères <t.bigueres@gmail.com>
2014-10-30 10:55:29 +0100

    #6 default sku for variation

--- classes/PowaTagProductAttributeHelper.php
+++ classes/PowaTagProductAttributeHelper.php
@@ -4,66 +4,8 @@ class PowaTagProductAttributeHelper {
 
 	public static function getCombinationByCode($id_product, $code)
 	{
-		$powatag_sku = Configuration::get('POWATAG_SKU');
-		switch ($powatag_sku) 
-		{
-			case Powatag::EAN :
-				$id_pa = (int)self::getCombinationIdByEan13($id_product, $code);	
-				break;
-			case Powatag::UPC :
-				$id_pa = (int)self::getCombinationIdByUPC($id_product, $code);
-			case Powatag::REFERENCE :
-				$id_pa = (int)self::getCombinationIdByReference($id_product, $code);
-			default:
-				$id_pa = (int)self::getCombinationIdByIdCombination($id_product, $code);
-				break;
-		}
-		if($id_pa == 0)
-			$id_pa = (int)self::getCombinationIdByIdCombination($id_product, $code);
-
+		$id_pa = (int)self::getCombinationIdByIdCombination($id_product, $code);
 		return $id_pa;
-	}
-
-	private static function getCombinationIdByEan13($id_product, $ean13)
-	{
-		if (empty($ean13))
-			return 0;
-
-		$query = new DbQuery();
-		$query->select('pa.id_product_attribute');
-		$query->from('product_attribute', 'pa');
-		$query->where('pa.ean13 = \''.pSQL($ean13).'\'');
-		$query->where('pa.id_product = '.(int)$id_product);
-
-		return Db::getInstance()->getValue($query);
-	}
-
-	private static function getCombinationIdByUpc($id_product, $upc)
-	{
-		if (empty($upc))
-			return 0;
-
-		$query = new DbQuery();
-		$query->select('pa.id_product_attribute');
-		$query->from('product_attribute', 'pa');
-		$query->where('pa.upc = \''.pSQL($upc).'\'');
-		$query->where('pa.id_product = '.(int)$id_product);
-
-		return Db::getInstance()->getValue($query);
-	}
-
-	private static function getCombinationIdByReference($id_product, $reference)
-	{
-		if (empty($reference))
-			return 0;
-
-		$query = new DbQuery();
-		$query->select('pa.id_product_attribute');
-		$query->from('product_attribute', 'pa');
-		$query->where('pa.reference = \''.pSQL($reference).'\'');
-		$query->where('pa.id_product = '.(int)$id_product);
-
-		return Db::getInstance()->getValue($query);
 	}
 
 	private static function getCombinationIdByIdCombination($id_product, $id_combination)
